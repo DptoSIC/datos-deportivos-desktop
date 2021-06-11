d45a78894995a4c8976a250044c904eed034009f
Awes0meM4n <lonchezas@hotmail.com>
2021-06-11 13:12:31 +0200

    Usando TableLayout

--- src/main/java/es/lanyu/desktop/App.java
+++ src/main/java/es/lanyu/desktop/App.java
@@ -1,9 +1,9 @@
 package es.lanyu.desktop;
 
-import java.awt.BorderLayout;
-import java.awt.Label;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.esotericsoftware.tablelayout.swing.Table;
 
 public class App {
 
@@ -11,10 +11,17 @@ public class App {
     int ancho = 480, alto = 270;
     JFrame frame = new JFrame("Mi Frame");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    frame.setLayout(null); // No se recomienda
-    System.out.println(frame.getLayout());
-    frame.getContentPane().add(new Label("Mi Etiqueta"), BorderLayout.SOUTH);
     
+    Table tabla = new Table();
+//    frame.add(tabla);
+    frame.getContentPane().add(tabla);
+    
+    tabla.addCell(new JLabel("Mi etiqueta")).width(ancho/2);
+    tabla.addCell(new JLabel("otra"));
+    tabla.row();
+    tabla.addCell(new JLabel("Otra fila"));
+    
+    tabla.debug();
 //    frame.show();
     frame.setSize(ancho, alto);
     frame.setVisible(true);
